5e00fb07091e38bebbd1051ce12681e5f50624ff
okazari <plouzennec.benjamin@gmail.com>
2022-02-25 20:47:36 +0100

    ➕ Added @welcome-ui/core and @welcome-ui/button

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,10 +2,14 @@ import App from "./App";
 import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     render(<App />);
   });
   it("should display a welcome message", () => {
     expect(screen.getByRole("main")).toHaveTextContent("Welcome");
+  });
+
+  it("should display a button", () => {
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 });
