7a4949ffc378791c380e642b9082a00a5d400028
okazari <plouzennec.benjamin@gmail.com>
2022-02-25 22:18:57 +0100

    💄 Added first layout with search input

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,35 @@
 import { createTheme, WuiProvider } from "@welcome-ui/core";
-import { Button } from "@welcome-ui/button";
-
+import { InputText } from "@welcome-ui/input-text";
+import { Box } from "@welcome-ui/box";
+import { Field } from "@welcome-ui/field";
+import { FC } from "react";
+import { SearchIcon } from "@welcome-ui/icons.search";
+import { Stack } from "@welcome-ui/stack";
 const theme = createTheme();
+
+const AppContainer: FC = ({ children }) => (
+  <Box
+    display="grid"
+    gridTemplateRows="min-content 1fr"
+    padding="xxl"
+    h="100vh"
+  >
+    <Stack spacing="md">{children}</Stack>
+  </Box>
+);
 
 const App = () => {
   return (
     <WuiProvider theme={theme}>
-      <div className="App" role="main">
-        Welcome <Button variant="primary">Primary</Button>
-      </div>
+      <AppContainer>
+        <Field label="Your dream job ?">
+          <InputText
+            aria-label="search for your dream job"
+            icon={<SearchIcon />}
+            isClearable
+          />
+        </Field>
+      </AppContainer>
     </WuiProvider>
   );
 };
